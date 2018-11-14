834d498c84a5ac7b70056d864f636cda2f9a0f3b
jpwmaher <jonmaher@me.com>
2018-11-14 12:38:27 +0000

    s

--- include-popertee-masthead.php
+++ include-popertee-masthead.php
@@ -1,4 +1,4 @@
 <div class="cell">
     <a href="index.php"><img src="img/poperteeLogoWhite.svg" style="height: 40px; padding: 5px 0px;"></a>
-    <a href="#" class="masthead-link">Log in</a> &nbsp; <a href="#" class="masthead-link">Sign up</a>&nbsp;
+    <a href="#" class="masthead-link">Log in</a> &nbsp; <a href="#" class="masthead-link">Sign up</a>&nbsp;|&nbsp;
 </div>
