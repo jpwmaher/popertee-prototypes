cd2baaa1c3aa813f75f285d658bc270e6798cce7
jpwmaher <jonmaher@me.com>
2018-11-14 16:13:18 +0000

    s

--- 2a-map.php
+++ 2a-map.php
@@ -58,7 +58,15 @@
                     <a class="tiny button">Not important</a>
                 </div>
             </div>
-            <div class="cell">Age</div>
+            <div class="cell"><label>Age</label>
+                <div class="slider" data-slider data-initial-start="25" data-initial-end="75">
+                    <span class="slider-handle" data-slider-handle role="slider" tabindex="1"></span>
+                    <span class="slider-fill" data-slider-fill></span>
+                    <span class="slider-handle" data-slider-handle role="slider" tabindex="1"></span>
+                    <input type="hidden">
+                    <input type="hidden">
+                </div>
+            </div>
             <div class="cell">Earnings</div>
             <div class="cell">Features</div>
         </div>
