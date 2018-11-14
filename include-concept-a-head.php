51a5ff2a443edb4c3757103cfeefb817dd66a50c
jpwmaher <jonmaher@me.com>
2018-11-14 10:47:34 +0000

    pop

--- include-concept-a-head.php
+++ include-concept-a-head.php
@@ -1,7 +1,6 @@
-<title>Long term concepts - SCRa</title>
+<title>Popertee prototypes</title>
 <link rel="stylesheet" href="css/foundation.css">
 <link rel="stylesheet" href="css/app.css">
-<link rel="stylesheet" href="css/scra.css">
-<link rel="stylesheet" href="css/scra-concepts.css">
+<link rel="stylesheet" href="css/popertee.css">
 <link rel="stylesheet" href="css/foundation-icons.css" />
 <script src="https://www.w3schools.com/lib/w3.js"></script>
