03afc03512de748ca9657792604d2a996a4a566a
jpwmaher <jonmaher@me.com>
2018-11-14 11:01:09 +0000

    index

--- index.php
+++ index.php
@@ -1,1 +1,42 @@
-<?php header( 'Location: /index.html' ) ;  ?>
+<html class="no-js" lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="format-detection" content="telephone=no">
+    <?php include("include-popertee-head.php"); ?>
+</head>
+
+<?php include("variables-popertee.php"); ?>
+
+<body>
+
+    <!-- Grid container -->
+    <div class="grid-container">
+
+        <div class="grid-x grid-padding-x">
+            <div id="page-head" class="cell">
+                <h1>Popertee</h1>
+            </div>
+        </div>
+
+        <div class="grid-x grid-padding-x">
+            <div id="research" class="cell" style="margin-top: 40px;">
+                <h2>Research</h2><br/>
+                <div class="grid-x grid-padding-x grid-margin-x small-up-1 medium-up-3 large-up-4">
+                    <div class="cell nav-panel"><a class="button hollow expanded big-font" href="132-research-done.html">Summary</a></div>
+                    <div class="cell nav-panel"><a class="button hollow expanded big-font" href="132-research-videos.html">Videos</a></div>
+                    <div class="cell nav-panel"><a class="button hollow expanded big-font" href="https://confluence.digital.nhs.uk/display/PT/132+-+SCRa+Discovery" target="_blank">Confluence pages</a></div>
+                </div>
+            </div>
+        </div>
+
+        <!-- End of grid container -->
+    </div>
+
+    <?php include("include-popertee-javascripts.php"); ?>
+
+</body>
+
+</html>
