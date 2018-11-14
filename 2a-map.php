a9c48809a5b7d4b2b5e1b1a92cb77eda97b88318
jpwmaher <jonmaher@me.com>
2018-11-14 16:15:07 +0000

    s

--- 2a-map.php
+++ 2a-map.php
@@ -67,8 +67,16 @@
                     <input type="hidden">
                 </div>
             </div>
-            <div class="cell">Earnings</div>
-            <div class="cell">Features</div>
+            <div class="cell"><label>Earnings</label>
+                <div class="slider" data-slider data-initial-start="25" data-initial-end="75">
+                    <span class="slider-handle" data-slider-handle role="slider" tabindex="1"></span>
+                    <span class="slider-fill" data-slider-fill></span>
+                    <span class="slider-handle" data-slider-handle role="slider" tabindex="1"></span>
+                    <input type="hidden">
+                    <input type="hidden">
+                </div>
+            </div>
+            <div class="cell">Interests</div>
         </div>
 
 
