a707a5712fd726328f223c5660a7a973b64edee3
jpwmaher <jonmaher@me.com>
2018-11-14 14:07:49 +0000

    s

--- 2a-homepage.php
+++ 2a-homepage.php
@@ -35,14 +35,18 @@
             </div>
 
             <div class="cell">
-                <fieldset>
-                    <legend>Choose Your Favorite</legend>
-                    <input type="radio" name="pokemon" value="Red" id="pokemonRed" required><label for="pokemonRed">Red</label>
-                    <input type="radio" name="pokemon" value="Blue" id="pokemonBlue" data-toggle-focus="form-callout"><label for="pokemonBlue">Blue</label>
-                    <input type="radio" name="pokemon" value="Yellow" id="pokemonYellow"><label for="pokemonYellow">Yellow</label>
-                </fieldset>
-                <div class="secondary callout is-hidden" id="form-callout" data-toggler="is-hidden">
-                    <p>This is only visible when the above field has focus.</p>
+                <ul class="tabs" data-tabs id="example-tabs">
+                    <li class="tabs-title is-active"><a href="#panel1" aria-selected="true">Tab 1</a></li>
+                    <li class="tabs-title"><a data-tabs-target="panel2" href="#panel2">Tab 2</a></li>
+                </ul>
+            </div>
+
+            <div class="tabs-content" data-tabs-content="example-tabs">
+                <div class="tabs-panel is-active" id="panel1">
+                    <p>Vivamus hendrerit arcu sed erat molestie vehicula. Sed auctor neque eu tellus rhoncus ut eleifend nibh porttitor. Ut in nulla enim. Phasellus molestie magna non est bibendum non venenatis nisl tempor. Suspendisse dictum feugiat nisl ut dapibus.</p>
+                </div>
+                <div class="tabs-panel" id="panel2">
+                    <p>Suspendisse dictum feugiat nisl ut dapibus. Vivamus hendrerit arcu sed erat molestie vehicula. Ut in nulla enim. Phasellus molestie magna non est bibendum non venenatis nisl tempor. Sed auctor neque eu tellus rhoncus ut eleifend nibh porttitor.</p>
                 </div>
             </div>
 
