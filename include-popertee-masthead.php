57b387e6026278b520caec879be36aa33320392a
jpwmaher <jonmaher@me.com>
2018-11-14 12:09:25 +0000

    d

--- include-popertee-masthead.php
+++ include-popertee-masthead.php
@@ -1,4 +1,4 @@
 <div class="cell">
-    <a href="index.php"><img src="img/poperteeLogoWhite.svg" style="height: 30px; padding: 5px 0px;"></a>
-    <a href="#" style="float: right; padding: 5px 0px;">Log in</a>
+    <a href="index.php"><img src="img/poperteeLogoWhite.svg" style="height: 50px; padding: 5px 0px;"></a>
+    <a href="#" style="float: right; padding: 5px 0px;">Log in</a><a href="#" style="float: right; padding: 5px 0px;">Sign up</a>
 </div>
