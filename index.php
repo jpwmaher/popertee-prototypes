33676ad9e407e51a374b0d119516203238ef8c57
jpwmaher <jonmaher@me.com>
2018-11-15 12:06:44 +0000

    x

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
         <div class="grid-x grid-padding-x">
             <div id="research" class="cell" style="margin-top: 40px; color: white !important;">
                 <h2>Prototypes</h2><br/>
-                <div class="grid-x grid-padding-x grid-margin-x small-up-1 medium-up-1 large-up-1">
+                <div class="grid-x grid-padding-x grid-margin-x small-up-1 medium-up-2 large-up-3">
                     <div class="cell nav-panel"><a class="button hollow secondary expanded " href="1a-homepage.php">Version 1 - Sam's doc</a></div>
                     <div class="cell nav-panel"><a class="button hollow secondary expanded " href="2a-homepage.php">Version 2a - Audience questions asked up-front</a></div>
                     <div class="cell nav-panel"><a class="button hollow secondary expanded " href="2b-homepage.php">Version 2b - No audience questions</a></div>
